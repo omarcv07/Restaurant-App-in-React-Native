f0d306bb8fe674e6f3057c14cb20f06074dd8576
omarcv07 <omarenriquect@gmail.com>
2020-08-13 19:36:55 -0400

    Components Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import Menu from './MenuComponent';
+import Dishdetail from './DishDetailComponent';
+import { View } from 'react-native';
 import { DISHES } from '../shared/dishes';
 
 const Main = () => {
-    const [dishes, setDishes] = useState(DISHES)
+  const [dishes, setDishes] = useState(DISHES)
+  const [selectedDish, setSelectedDish] = useState(null)
+ 
+  const onDishSelect = (dishId) => setSelectedDish(dishId)
 
-    return (
-        <Menu dishes={dishes} />
-    );
+  return (
+    <View>
+      <Menu dishes={dishes} onPress={(dishId) => onDishSelect(dishId)} />
+      <Dishdetail dish={dishes.find((dish) => dish.id === selectedDish)} />
+    </View>
+  );
 }
-
+  
 export default Main;
